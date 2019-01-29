5966866c666ffbde884e06dd5a6ba243699373c9
Elijah <ehager@nd.edu>
2019-01-29 18:53:19 -0500

    same

--- vivagua_app/lib/main.dart
+++ vivagua_app/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
+import './Pages/landing_page.dart';
+void main(){
+  runApp(new MaterialApp(
+    home: new LandingPage(),
+  ));
+} 
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
