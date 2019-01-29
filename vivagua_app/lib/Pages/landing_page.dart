99ec6024b150e207155ed306818de77fbb4e30e5
Elijah <ehager@nd.edu>
2019-01-29 18:53:10 -0500

    blank landing page

---
+++ vivagua_app/lib/Pages/landing_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){ // build a widget
+    return new Material(              // that widget will be a material
+      color: Colors.greenAccent,      // with color green
+    );
+  }
+}
