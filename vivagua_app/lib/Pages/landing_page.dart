531c6e680c12778c665e165c3a2c89ce5279b74a
Elijah <ehager@nd.edu>
2019-02-02 18:44:45 -0500

    Button added

--- vivagua_app/lib/Pages/landing_page.dart
+++ vivagua_app/lib/Pages/landing_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
 
 class LandingPage extends StatelessWidget {
   @override
@@ -9,18 +11,43 @@ class LandingPage extends StatelessWidget {
 
         children: <Widget>[
           Container(
-            decoration: BoxDecoration(image: DecorationImage(
+            decoration: new BoxDecoration(image: new DecorationImage(
               image: AssetImage('assets/img/home_background.png'), fit: BoxFit.fill)
             ),
-          )
+          ),
 
-
+          Positioned(child: Image.asset('assets/img/Vivagua logo.png', scale: 1.8), top: 200, left: 190),
+          
+          Positioned(child: new StartButton(), top: 600, left: 200),
 
         ],
 
 
 
       )
+    );
+  }
+}
+
+class StartButton extends StatefulWidget{
+  @override
+  StartButtonState createState() => new StartButtonState();
+}
+class StartButtonState extends State<StartButton>{
+
+  void onPressed(){
+    setState((){
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return new RaisedButton(
+
+      child: new Text("Log a Dive!", style: new TextStyle(color: Colors.white, fontSize: 32, fontStyle: FontStyle.italic)),
+      color: const Color(0xff33ebff),
+      onPressed: onPressed,
     );
   }
 }
