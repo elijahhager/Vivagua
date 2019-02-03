abe8b9163cb08ae199ea52dc34c3a606d817b300
Elijah <ehager@nd.edu>
2019-02-02 20:28:34 -0500

    dive site page and routes added

---
+++ vivagua_app/lib/Pages/dive_site_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class DiveSitePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){ // build a widget
+
+  }
+    
+}
--- vivagua_app/lib/main.dart
+++ vivagua_app/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import './Pages/landing_page.dart';
+import './Pages/dive_site_page.dart';
 
 void main(){
   runApp(new MaterialApp(
     debugShowCheckedModeBanner: false,
     home: new LandingPage(),
+    routes: <String, WidgetBuilder>{
+      "/DiveSitePage": (BuildContext context) => new DiveSitePage()
+    }
   ));
 } 
